a00609a9b637f7cd765528d6af3eed29efa86553
Hypocrisy <xuweiiam@gmail.com>
2015-09-10 23:57:46 +0800

    change the about and contact tags.

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -25,6 +25,23 @@ function init() {
 
 init();
 
+$("#about").popover({
+    trigger: "click",
+    animation: "true",
+    delay: { "show": 100, "hide": 100 },
+    placement: "right",
+    title: "About",
+    content: "我既无未来，也无往昔。若我羁留于此，我便羁留于前行。若我前行，前路上即有我的羁留。惟爱与死能将其改变。"
+});
+
+$("#contact").popover({
+    trigger: "click",
+    animation: "true",
+    placement: "right",
+    title: "Contact",
+    content: "Please contact me by xuweiiam@gmail.com."
+});
+
 $("#allLinks").click(function() {
     $("div.main h2.sub-header").text("All Links");
     //$('#links').remove();   //删除选中元素
